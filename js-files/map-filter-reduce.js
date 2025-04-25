8f6845bb9d739c7b599de302aa1b51d5158712e9
Praveesh <praveesh91@gmail.com>
2025-04-25 23:24:32 +0530

    add filter and reduce polyfill

--- js-files/map-filter-reduce.js
+++ js-files/map-filter-reduce.js
@@ -10,4 +10,30 @@ Array.prototype.myMap = function (cb) {
   return res;
 };
 
-console.log([1, 2, 3, 4, 5].myMap((el) => el * 2));
+Array.prototype.myFilter = function (cb) {
+  if (typeof cb !== "function") {
+    throw new Error("Callback is not a function");
+  }
+  let res = [];
+  let array = this;
+  for (let i = 0; i < array.length; i++) {
+    if (cb(array[i], i, array)) {
+      res.push(array[i]);
+    }
+  }
+  return res;
+};
+
+Array.prototype.myReduce = function (cb, init) {
+  if (typeof cb !== "function") {
+    throw new Error("Callback is not a function");
+  }
+  let array = this;
+  let accumulator = init !== undefined ? init : array[0];
+  for (let i = init !== undefined ? 0 : 1; i < array.length; i++) {
+    accumulator = cb(accumulator, array[i], i, array);
+  }
+  return accumulator;
+};
+
+console.log([1, 2, 3, 4, 5].myReduce((init, acc) => init + acc, 0));
