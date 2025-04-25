ba0fcd9222815864fd0a0fbdcb9afdba7db162e5
Praveesh <praveesh91@gmail.com>
2025-04-26 01:38:15 +0530

    add caal apply bind polyfill

---
+++ js-files/call-apply-bind.js
@@ -0,0 +1,32 @@
+Function.prototype.myCall = function (context, ...args) {
+  if (typeof this !== "function") {
+    throw new Error(this + "is not callable");
+  }
+  context.fn = this;
+  return context.fn(args);
+};
+
+Function.prototype.myApply = function (context, args) {
+  if (typeof this !== "function") {
+    throw new Error(this + "is not callable");
+  }
+  context.fn = this;
+  return context.fn(...args);
+};
+
+Function.prototype.myBind = function (context, ...args) {
+  if (typeof this !== "function") {
+    throw new Error(this + "is not callable");
+  }
+  context.fn = this;
+  return function (...extraArgs) {
+    return context.fn(args, ...extraArgs);
+  };
+};
+
+let obj = { name: "John Doe" };
+function sayName(age) {
+  return `My name is ${this.name} and I am ${age} old`;
+}
+let bound = sayName.myBind(obj, 23);
+console.log(bound());
