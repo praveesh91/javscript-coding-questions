c08a0718f53990cacea0316b28d260489c983f73
Praveesh <praveesh91@gmail.com>
2025-04-26 16:43:04 +0530

    add currying

---
+++ js-files/currying.js
@@ -0,0 +1,27 @@
+function sum(a, b) {
+  return function (b) {
+    if (b === undefined) {
+      return a;
+    }
+    return sum(a + b);
+  };
+}
+
+// console.log(sum(1)(2)(3)(4)(5)());
+
+function curry(func) {
+  return function curried(...args) {
+    if (args.length >= func.length) {
+      return func(...args);
+    } else {
+      return function (...args2) {
+        return curried(...args, ...args2);
+      };
+    }
+  };
+}
+
+const sumCurry = (a, b, c) => a + b + c;
+
+const curriedSum = curry(sumCurry);
+console.log(curriedSum(1)(6)(5));
