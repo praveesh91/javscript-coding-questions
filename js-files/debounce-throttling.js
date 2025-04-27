9beeaa27910709814ad4995dc4beb39839ad259f
praveesh91 <praveesh91@gmail.com>
2025-04-27 12:24:39 +0530

    add formatting

--- js-files/debounce-throttling.js
+++ js-files/debounce-throttling.js
@@ -2,6 +2,9 @@ let debounceFn = debounce((el) => console.log(el.target.value), 1000);
 const inputEl = document.getElementById("username");
 inputEl.addEventListener("input", debounceFn);
 
+let myThrottle = throttle(() => console.log("demo"), 2000);
+window.addEventListener("mousemove", myThrottle);
+
 function debounce(fn, delay) {
   let timeoutId;
   return function (...args) {
@@ -9,9 +12,6 @@ function debounce(fn, delay) {
     timeoutId = setTimeout(() => fn.apply(this, args), delay);
   };
 }
-
-let myThrottle = throttle(() => console.log("demo"), 2000);
-window.addEventListener("mousemove", myThrottle);
 
 function throttle(fn, delay) {
   let start = 0;
