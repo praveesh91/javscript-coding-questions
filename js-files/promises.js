58cdb75a2d006e4964e9f49ba49269ec417d85d7
praveesh91 <praveesh91@gmail.com>
2025-04-27 23:20:24 +0530

    add promise then

--- js-files/promises.js
+++ js-files/promises.js
@@ -2,27 +2,19 @@ const samplePromise = new myPromise((resolve, reject) =>
   setTimeout(() => resolve("Resolved"), 2000)
 );
 
-samplePromise
-  .then((data) => console.log(data))
-  .catch((data) => console.log(data));
+samplePromise.then((data) => console.log(data));
 
 function myPromise(executor) {
-  let onResolve, onReject;
+  let onResolve;
 
   function resolve(value) {
     onResolve(value);
-  }
-  function reject() {
-    onReject(value);
   }
 
   this.then = function (callback) {
     onResolve = callback;
     return this;
   };
-  this.catch = function (callback) {
-    onReject = callback;
-    return this;
-  };
-  executor(resolve, reject);
+
+  executor(resolve);
 }
