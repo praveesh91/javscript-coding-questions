36a8b035d039ffc4c3f6ca5eece4aa3fefc7cd59
praveesh91 <praveesh91@gmail.com>
2025-05-02 22:39:56 +0530

    flatten nested array

---
+++ js-files/flatten-array.js
@@ -0,0 +1,18 @@
+const arr = [1, 2, [3, 4, [5, 6]]];
+
+console.log(flattenArr(arr));
+
+function flattenArr(arr) {
+  let res = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (Array.isArray(arr[i])) {
+      let flatten = flattenArr(arr[i]);
+      for (let j = 0; j < flatten.length; j++) {
+        res.push(flatten[j]);
+      }
+    } else {
+      res.push(arr[i]);
+    }
+  }
+  return res;
+}
