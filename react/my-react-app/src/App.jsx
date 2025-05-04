f76e06c57bebabcba0939fa594a53e48f16a94e8
praveesh91 <praveesh91@gmail.com>
2025-05-05 02:36:36 +0530

    add useEffect useLayoutEffect

--- react/my-react-app/src/App.jsx
+++ react/my-react-app/src/App.jsx
@@ -2,13 +2,15 @@ import "./App.css";
 import withCounter from "./component-types/withCounter.jsx";
 import Counter from "./component-types/Counter.jsx";
 import Lifecycle from "./Lifecycle.jsx";
+import UseEffectHook from "./UseEffectHook.jsx";
 
 const EnhancedCOunter = withCounter(Counter);
 function App() {
   return (
     <>
       {/* <EnhancedCOunter /> */}
-      <Lifecycle />
+      {/* <Lifecycle /> */}
+      <UseEffectHook />
     </>
   );
 }
