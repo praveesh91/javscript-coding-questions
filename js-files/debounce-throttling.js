a17557c766f404c3ccca65e9c6221b7a04d245ce
praveesh91 <praveesh91@gmail.com>
2025-07-26 23:30:52 +0530

    variable name change

--- js-files/debounce-throttling.js
+++ js-files/debounce-throttling.js
@@ -14,11 +14,11 @@ function debounce(fn, delay) {
 }
 
 function throttle(fn, delay) {
-  let start = 0;
+  let last = 0;
   return function (...args) {
     let now = new Date().getTime();
-    if (now - start < delay) return;
-    start = now;
+    if (now - last < delay) return;
+    last = now;
     return fn.apply(this, args);
   };
 }
