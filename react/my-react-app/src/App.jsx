21737bae308cdc3e8f0aef50180719845b8e1684
praveesh91 <praveesh91@gmail.com>
2025-08-04 14:27:33 +0530

    add titactoe game

--- react/my-react-app/src/App.jsx
+++ react/my-react-app/src/App.jsx
@@ -5,6 +5,7 @@ import MemoryGame from "./components/memory-game/MemoryGame";
 import Pagination from "./components/pagination/Pagination";
 import Progressbar from "./components/progressbar/Progressbar";
 import Tab from "./components/tab/Tab";
+import TicTacToe from "./components/tictactoe/TicTacToe";
 import Virtualisation from "./components/virtualisation/Virtualisation";
 import useNotification from "./hooks/use-notification";
 
@@ -30,7 +31,8 @@ function App() {
       {/* <Progressbar maxValue={200} duration={5000} /> */}
       {/* <Virtualisation /> */}
       {/* <Carousel /> */}
-      <MemoryGame />
+      {/* <MemoryGame /> */}
+      <TicTacToe />
     </>
   );
 }
