63d438f6fba6925c12491534f79c287c2a256705
praveesh91 <praveesh91@gmail.com>
2025-08-24 23:58:19 +0530

    add remove and add services

--- react/my-react-app/src/db/api.ts
+++ react/my-react-app/src/db/api.ts
@@ -8,7 +8,52 @@ export const fetchPoll = async (pollId: number): Promise<Poll> => {
   return response.json();
 };
 
+export const submitVote = async (
+  pollId: number,
+  selectOptions: number[]
+): Promise<Option[]> => {
+  const poll = await fetchPoll(pollId);
+  const updatedOptions = poll.options.map((option) => {
+    if (selectOptions.includes(option.id)) {
+      return { ...option, votes: option.votes + 1 };
+    }
+    return option;
+  });
+  const totalCount = updatedOptions.reduce(
+    (acc, option) => acc + option.votes,
+    0
+  );
+  await fetch(`http://localhost:3000/polls/${pollId}`, {
+    method: "PATCH",
+    headers: {
+      "Content-type": "application/json",
+    },
+    body: JSON.stringify({ options: updatedOptions, totalCount }),
+  });
+  return updatedOptions;
+};
+
 export const removeVote = async (
   pollId: number,
-  selectOptions: number
-): Promise<Option> => {};
+  selectOptions: number[]
+): Promise<Option[]> => {
+  const poll = await fetchPoll(pollId);
+  const updatedOptions = poll.options.map((option) => {
+    if (selectOptions.includes(option.id)) {
+      return { ...option, votes: option.votes > 0 ? option.votes - 1 : 0 };
+    }
+    return option;
+  });
+  const totalCount = updatedOptions.reduce(
+    (acc, option) => acc + option.votes,
+    0
+  );
+  await fetch(`http://localhost:3000/polls/${pollId}`, {
+    method: "PATCH",
+    headers: {
+      "Content-type": "application/json",
+    },
+    body: JSON.stringify({ options: updatedOptions, totalCount }),
+  });
+  return updatedOptions;
+};
