25193804677e6d5872c67b167ca7248347b31e58
praveesh91 <praveesh91@gmail.com>
2025-09-06 01:27:30 +0530

    get input and move to next input field

--- react/my-react-app/src/components/otp/Otp.jsx
+++ react/my-react-app/src/components/otp/Otp.jsx
@@ -1,12 +1,35 @@
-import React from "react";
-import useCycle from "../../hooks/useCycle";
+import React, { useRef, useState } from "react";
 
-const Otp = () => {
-  const [mode, cycle] = useCycle("one", "two", "three");
+const Otp = ({ otpLength = 6 }) => {
+  const [otpFields, setOtpFields] = useState(new Array(otpLength).fill(""));
+  const ref = useRef([]);
+
+  console.log(ref);
+
+  const handleKeydown = (event, index) => {
+    if (isNaN(event.key)) {
+      return;
+    }
+    const copyOtpFields = [...otpFields];
+    copyOtpFields[index] = event.key;
+    ref.current[index + 1].focus();
+    setOtpFields(copyOtpFields);
+  };
 
   return (
-    <div className="flex align-middle justify-center" onClick={cycle}>
-      {mode}
+    <div className="">
+      {otpFields.map((el, index) => {
+        return (
+          <input
+            ref={(currentInput) => (ref.current[index] = currentInput)}
+            className="w-10 h-10 text-center text-xl border border-gray-300 rounded-md focus:border-blue-500"
+            key={index}
+            value={el}
+            type="text"
+            onKeyDown={(event) => handleKeydown(event, index)}
+          />
+        );
+      })}
     </div>
   );
 };
